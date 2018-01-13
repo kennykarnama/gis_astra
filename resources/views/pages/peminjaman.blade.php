9e8a056ebec415ebd1ba37683b2e2a0e319c3a94
kennykarnama <kennykarnama@gmail.com>
2018-01-13 11:24:53 +0700

    add attribute belowOrigin dropdown

---
+++ resources/views/pages/peminjaman.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+
+@section('content')
+
+<div class="row">
+	<div class="col s8">
+		<a class="waves-effect waves-light btn">Tambah Data Peminjaman</a>
+
+	</div>
+</div>
+
+@stop
+
+@push('scripts')
+
+@endpush
