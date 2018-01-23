672fd666d05eab0cd2c68487e2645c9b5192fa04
zafitra <zafit.ra789@gmail.com>
2018-01-23 10:44:24 +0700

    menambahkan tampilan eod

--- resources/views/pages/pembayaran.blade.php
+++ resources/views/pages/pembayaran.blade.php
@@ -41,45 +41,75 @@
         <p class="center-align"><b>Data Customer</b></p>
               <div class="row">
 
+
+
     <div class="input-field col s12">
-          <input id="no_agreement" type="text" class="validate">
+          <input disabled id="no_agreement" type="text" class="validate">
           <label for="no_agreement">No Agreement</label>
       </div>
       <div class="input-field col s12">
-          <input id="nama_peminjam" type="text" class="validate">
+          <input disabled id="nama_peminjam" type="text" class="validate">
           <label for="nama_peminjam">Nama Peminjam</label>
       </div>
      
       <div class="input-field col s12">
-          <input id="jumlah_angsuran" type="number" class="validate">
+          <input disabled id="jumlah_angsuran" type="number" class="validate">
           <label for="jumlah_angsuran">Jumlah Angsuran</label>
       </div>
        <div class="input-field col s12">
-          <input id="nilai_angsuran" type="number" class="validate">
+          <input disabled id="nilai_angsuran" type="number" class="validate">
           <label for="nilai_angsuran">Nilai Angsuran</label>
       </div>
-      <div class="input-field col s12">
-          <input id="saldo" type="number" class="validate">
-          <label for="saldo">Saldo</label>
+
+      <div class="center-align">
+          
+           <a class="waves-effect waves-light btn" id="btn-bayar">Bayar</a>
+
       </div>
 
-      
-      <ul id="dropdown2" class="dropdown-content">
-    <li><a href="#!">Terbayar</a></li>
-    <li><a href="#!" id="link-kurang">Kurang</a></li>
-  </ul>
-  <a class="btn dropdown-button" href="#!" data-activates="dropdown2">EOD<i class="material-icons right">arrow_drop_down</i></a>
-            
+      <div class="collection-item">
+       <ul class="collection with-header">
+        <li class="collection-header"><p><b>Kurang</b></p></li>
+        <div>Januari<a href="#!" id="link-bayar" class="secondary-content">Bayar</a>
+        </div>
+       </ul>
+      </div>
+
+        <!-- Modal bayar Structure -->
+  <div id="modal-bayar" class="modal">
+    <div class="modal-content">
+
+    <div class="row">
+    
+      <div class="input-field col s12">
+          <input id="tgl_jatuh_tempo" type="text" class="datepicker">
+          <label for="tgl_jatuh_tempo">Tanggal Pembayaran</label>
+      </div>
+      <div class="input-field col s12">
+          <input id="nilai_angsuran_bayar" type="number" class="validate">
+          <label for="nominal_bayar">Nominal Pembayaran</label>
+      </div>
+
+    </div>
+
+    </div>
+
+    
+    <div class="modal-footer">
+      <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Simpan</a>
+    </div>
+  </div>
+          
 
     </div>
         </div>
         <div class="card-action">
 
-        <div class="center-align">
+        <!-- <div class="center-align">
         	
         	 <a class="waves-effect waves-light btn">Simpan</a>
 
-        </div>
+        </div> -->
         
         </div>
       </div>
@@ -92,29 +122,23 @@
 
 </div>
 
- <!-- Modal EOD Kurang -->
-  <div id="modal_eod_kurang" class="modal modal-fixed-footer">
-    <div class="modal-content">
-      <h4>Pembayaran yang perlu dilakukan sejumlah</h4>
-      <br>
-      <br>
-      <br>
-      <h2 class="center-align"><b>Rp. 14.500.000,-</b></h2>
-    </div>
-    <div class="modal-footer">
-      <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Agree</a>
-    </div>
-  </div>
-          
+
+
+
+
 @push('scripts')
 	<script type="text/javascript">
 		
 		$(document).ready(function () {
 			// body...
-			$('#link-kurang').click(function () {
-			// body...
-			$('#modal_eod_kurang').modal('open');
-		});
+
+      $('#link-bayar').click(function () {
+        $('#modal-bayar').modal('open');
+      });
+
+      $('#btn-bayar').click(function () {
+        $('#modal-bayar').modal('open');
+      });
 
       $('#btn-cari-tagihan').click(function  () {
         // body...
