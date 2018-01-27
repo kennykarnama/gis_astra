4c7234ef54f0298807b20643446a6e7ad6ad9e12
kennykarnama <kennykarnama@gmail.com>
2018-01-27 20:30:24 +0700

    coba lg

--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -32,198 +32,198 @@ class UploadFileController extends Controller
     	return view('pages.upload_file',[]);
     }
 
-    public function import_excel()
-    {
-    	# code...
-    		if(Input::hasFile('import_file')){
+  //   public function import_excel()
+  //   {
+  //   	# code...
+  //   		if(Input::hasFile('import_file')){
 
     		
-			$path = Input::file('import_file')->getRealPath();
+		// 	$path = Input::file('import_file')->getRealPath();
 
-			$prepared_data = array();
+		// 	$prepared_data = array();
 
 				
 			
-			DB::table('report')->truncate();
+		// 	DB::table('report')->truncate();
 
-			DB::disableQueryLog();
+		// 	DB::disableQueryLog();
 		
 
-			Excel::filter('chunk')->load($path)->chunk(1000, function($results)
-			{
+		// 	Excel::filter('chunk')->load($path)->chunk(1000, function($results)
+		// 	{
 
 					
-					$counter = 0;
+		// 			$counter = 0;
 					
-						$prepared_data = array();
+		// 				$prepared_data = array();
 
-			        foreach ($results as $row) {
-			        	# code...
-			        	$headers = $row->keys()->all();
+		// 	        foreach ($results as $row) {
+		// 	        	# code...
+		// 	        	$headers = $row->keys()->all();
 
-			        	//dd($headers);
+		// 	        	//dd($headers);
 
-			        	$tmp = array();
+		// 	        	$tmp = array();
 
-			        	for($i=0;$i<count($headers);$i++){
+		// 	        	for($i=0;$i<count($headers);$i++){
 			        		
-			        		$col_name = $headers[$i];
+		// 	        		$col_name = $headers[$i];
 
 
 
-			        		$tmp[$col_name] = $row[$col_name];
+		// 	        		$tmp[$col_name] = $row[$col_name];
 
-			        	}
-
-
-			        	//echo $row->date_byr."<br>";
+		// 	        	}
 
 
+		// 	        	//echo $row->date_byr."<br>";
 
-			        	// $counter++;
 
-			        	array_push($prepared_data, $tmp);
+
+		// 	        	// $counter++;
+
+		// 	        	array_push($prepared_data, $tmp);
 
 			        	
 
 
-			        }
+		// 	        }
 
-			        //echo $counter."<br>";
+		// 	        //echo $counter."<br>";
 
-			        //dd($prepared_data);
-			        DB::table('report')->insert($prepared_data);
+		// 	        //dd($prepared_data);
+		// 	        DB::table('report')->insert($prepared_data);
 
-			       	//echo count($prepared_data)."<br>";
-			       // echo "sukses"."<br>";
+		// 	       	//echo count($prepared_data)."<br>";
+		// 	       // echo "sukses"."<br>";
 
 			      
-			});
+		// 	});
 
-			// $line0 = $data[0];
+		// 	// $line0 = $data[0];
 
-			// $headers = $line0->keys();
+		// 	// $headers = $line0->keys();
 
-			// $headers = $headers->all();
+		// 	// $headers = $headers->all();
 
-			// $prepared_data = array();
-			//$headers = $headers['items'];
-
-			// if(!empty($data) && $data->count()){
-			// 	foreach ($data as $key => $value) {
-
-			// 		$tmp = array();
-
-			// 		for($i=0; $i < count($headers); $i++){
-						
-			// 			$nama_kolom = $headers[$i];
-
-			// 			$tmp[$nama_kolom] = $value->$nama_kolom;
-			// 		}
-
-			// 		array_push($prepared_data, $tmp);
-
-			// 		//$insert[] = ['nama' => $value->nama, 'years' => $value->years];
-			// 	}
-			// 	if(!empty($prepared_data)){
-
-			// 		//dd($prepared_data);
-					
-			// 		$status = $this->insert_data_to_db_one_by_one('report',$prepared_data);
-
-			// 		if($status==1){
-			// 			$message = "Data berhasil diimport";
-			// 		}
-
-			// 		else{
-			// 			$message = "Data tidak berhasil diimport";
-			// 		}
-
-
-					 
-					
-			// 	}
-
-			// 	else{
-			// 		$message = "Terjadi error ";
-			// 	}
-
-
-			// }
-
-
-		}
-
-    }
-
-  //   public function import_excel()
-  //   {
-  //   	# code...
-  //   	if(Input::hasFile('import_file')){
-
-    		
-		// 	$path = Input::file('import_file')->getRealPath();
-		// 	$data = Excel::load($path, function($reader) {
-				
-		// 	})->get();
-
-		// 	$line0 = $data[0];
-
-		// 	$headers = $line0->keys();
-
-		// 	$headers = $headers->all();
-
-		// 	$prepared_data = array();
+		// 	// $prepared_data = array();
 		// 	//$headers = $headers['items'];
 
-		// 	if(!empty($data) && $data->count()){
-		// 		foreach ($data as $key => $value) {
+		// 	// if(!empty($data) && $data->count()){
+		// 	// 	foreach ($data as $key => $value) {
 
-		// 			$tmp = array();
+		// 	// 		$tmp = array();
 
-		// 			for($i=0; $i < count($headers); $i++){
+		// 	// 		for($i=0; $i < count($headers); $i++){
 						
-		// 				$nama_kolom = $headers[$i];
+		// 	// 			$nama_kolom = $headers[$i];
 
-		// 				$tmp[$nama_kolom] = $value->$nama_kolom;
-		// 			}
+		// 	// 			$tmp[$nama_kolom] = $value->$nama_kolom;
+		// 	// 		}
 
-		// 			array_push($prepared_data, $tmp);
+		// 	// 		array_push($prepared_data, $tmp);
 
-		// 			//$insert[] = ['nama' => $value->nama, 'years' => $value->years];
-		// 		}
-		// 		if(!empty($prepared_data)){
+		// 	// 		//$insert[] = ['nama' => $value->nama, 'years' => $value->years];
+		// 	// 	}
+		// 	// 	if(!empty($prepared_data)){
 
-		// 			//dd($prepared_data);
+		// 	// 		//dd($prepared_data);
 					
-		// 			$status = $this->insert_data_to_db_one_by_one('report',$prepared_data);
+		// 	// 		$status = $this->insert_data_to_db_one_by_one('report',$prepared_data);
 
-		// 			if($status==1){
-		// 				$message = "Data berhasil diimport";
-		// 			}
+		// 	// 		if($status==1){
+		// 	// 			$message = "Data berhasil diimport";
+		// 	// 		}
 
-		// 			else{
-		// 				$message = "Data tidak berhasil diimport";
-		// 			}
+		// 	// 		else{
+		// 	// 			$message = "Data tidak berhasil diimport";
+		// 	// 		}
 
 
 					 
 					
-		// 		}
+		// 	// 	}
 
-		// 		else{
-		// 			$message = "Terjadi error ";
-		// 		}
+		// 	// 	else{
+		// 	// 		$message = "Terjadi error ";
+		// 	// 	}
 
 
-		// 	}
+		// 	// }
+
+
 		// }
 
-		// Session::flash('pesan_import',$message);
-
-
-		// return back();
   //   }
+
+    public function import_excel()
+    {
+    	# code...
+    	if(Input::hasFile('import_file')){
+
+    		
+			$path = Input::file('import_file')->getRealPath();
+			$data = Excel::load($path, function($reader) {
+				
+			})->get();
+
+			$line0 = $data[0];
+
+			$headers = $line0->keys();
+
+			$headers = $headers->all();
+
+			$prepared_data = array();
+			//$headers = $headers['items'];
+
+			if(!empty($data) && $data->count()){
+				foreach ($data as $key => $value) {
+
+					$tmp = array();
+
+					for($i=0; $i < count($headers); $i++){
+						
+						$nama_kolom = $headers[$i];
+
+						$tmp[$nama_kolom] = $value->$nama_kolom;
+					}
+
+					array_push($prepared_data, $tmp);
+
+					//$insert[] = ['nama' => $value->nama, 'years' => $value->years];
+				}
+				if(!empty($prepared_data)){
+
+					//dd($prepared_data);
+					
+					$status = $this->insert_data_to_db_one_by_one('report',$prepared_data);
+
+					if($status==1){
+						$message = "Data berhasil diimport";
+					}
+
+					else{
+						$message = "Data tidak berhasil diimport";
+					}
+
+
+					 
+					
+				}
+
+				else{
+					$message = "Terjadi error ";
+				}
+
+
+			}
+		}
+
+		Session::flash('pesan_import',$message);
+
+
+		return back();
+    }
 
     private  function insert_data_to_db_one_by_one($table_name,$data)
     {
